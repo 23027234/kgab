ccf36e92d8a7018275ca938f5a6a804445ced60c
23027234 <166645833+23027234@users.noreply.github.com>
2024-04-18 20:46:03 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      /*HERE UM WRITING A CODE TO PRINT OUT LETTERS AND NUMBERS */
+      System.out.println("7");
+      System.out.println("A");
+      System.out.print("B");
+      System.out.print("C");
+    }
+}
